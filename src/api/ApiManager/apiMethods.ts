479d3e9868d5b590833164fef1d7150b2d00dc9e
esteban <esteban_silva11@hotmail.com>
2024-08-25 11:26:33 -0300

    fixing white page nro 1003

--- src/api/ApiManager/apiMethods.ts
+++ src/api/ApiManager/apiMethods.ts
@@ -14,8 +14,8 @@ const getHeaders = () => {
   };
 };
 
-const BASE_URL_BACKEND = import.meta.env.BASE_URL_BACKEND;
-console.log('URL BACK', import.meta.env.BASE_URL_BACKEND, process.env.BASE_URL_BACKEND);
+const BASE_URL_BACKEND = import.meta.env.VITE_BASE_URL_BACKEND;
+console.log('URL BACK', import.meta.env.VITE_BASE_URL_BACKEND, process.env.VITE_BASE_URL_BACKEND);
 
 class ApiMethods {
   static apiRequest = (
